fba200ef811d7fae571ee3ee2b0de0cb98f82fe1
danver <danyver98@gmail.com>
2024-10-11 14:30:00 +0300

    Fixed pylint

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,27 +1,30 @@
-from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi import HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from app.api.schemas.user import UserRegister, UserLogin
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import Annotated
+from app.api.schemas.user import UserRegister
 from app.db import operations
-from app.utils.singleton import Singleton
 from app.core.security import hash_password, verify_password, create_jwt_token
-from app.api.schemas import user
 
 # class AuthService(metaclass=Singleton):
 # We should create new service instance for each request (ain't good solution IMHO)
 class AuthService():
+    """
+        Service class for working with users
+    """
     def __init__(self, session: AsyncSession):
         self._session = session
-    
+
     async def register(self, data: UserRegister):
+        """Register user method"""
         data.password = hash_password(data.password)
         return await operations.create_user(self._session, data)
 
     async def get_user(self, id_: int):
+        """Get user method"""
         return await operations.get_user(self._session, id_)
 
     async def login(self, data: OAuth2PasswordRequestForm) -> str:
+        """Login user method"""
         db_user = await operations.get_user_by_login(self._session, data.username)
         if db_user is None:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User nt found")
@@ -37,4 +40,5 @@ class AuthService():
         return create_jwt_token(token_data)
 
     async def logout(self, login: str):
+        """Logout user method"""
         return await operations.logout_user(self._session, login)
